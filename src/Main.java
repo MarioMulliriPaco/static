d6649e36164d58bd3e9919113e32bccdcee1f8fd
Mario Mulliri Paco <mulliripacomario7425@gmail.com>
2022-09-30 10:09:23 -0400

    static

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[]argos){
+        System.out.println(Util.DOLARES);
+        double pesos;
+        pesos = Util.bolivianosPesos(1000);
+        System.out.println(pesos);
+    }
+}
---
+++ src/Util.java
@@ -0,0 +1,8 @@
+public class Util {
+    public static final double DOLARES = 6.96;
+    public static final double EUROS = 6.73;
+    public static final double ARGENTINOS = 21.33;
+    public static double bolivianosPesos(double bolivianos){
+        return bolivianos * ARGENTINOS;
+    }
+}
